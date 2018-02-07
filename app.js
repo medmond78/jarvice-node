3296136b96b1f088ccd550b7da2769f7db2f0a9b
Matthew <>
2018-02-07 21:31:58 +0000

    Moved job options to app.js

--- app.js
+++ app.js
@@ -6,8 +6,36 @@ console.log('Working...');
 console.log(process.env.username)
 console.log(process.env.apikey)
 
+var options = {
+	url: 'https://api.jarvice.com/jarvice/submit',
+	method: 'POST',
+	port: 443,
+	body:{"app": "migenius-irayviewer",
+	  "staging": false,
+	  "checkedout": false,
+	  "application": {
+	    "command": "irayviewer",
+	    "geometry": "1520x695"
+	  },
+	  "machine": {
+	    "type": "ng4",
+	    "nodes": 1
+	  },
+	  "vault": {
+	    "name": "drop.jarvice.com",
+	    "force": false,
+	    "readonly": false
+	  },
+	  "user": {
+	    "username": process.env.username, 
+	    "apikey": process.env.apikey
+	  }
+	},
+	json: true,
+};
+
         
-jarvice.submitJob();
+jarvice.submitJob(options);
 
 //jarvice.getJobs();
 
